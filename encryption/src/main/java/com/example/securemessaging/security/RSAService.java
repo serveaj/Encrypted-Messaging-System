a18f2c3f9aa72614b93b5ac31af0633a0e91794d
Ryder Maness <jmaness2@leomail.tamuc.edu>
2026-04-26 18:18:14 -0500

    Fixed a hashing mixup between KMS and Java

--- encryption/src/main/java/com/example/securemessaging/security/RSAService.java
+++ encryption/src/main/java/com/example/securemessaging/security/RSAService.java
@@ -1,13 +1,19 @@
 package com.example.securemessaging.security;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.MGF1ParameterSpec;
 
 public class RSAService {
 
     public byte[] encrypt(byte[] data, RSAPublicKey publicKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        OAEPParameterSpec oaepParams = new OAEPParameterSpec(
+            "SHA-256", "MGF1", MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT
+        );
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams);
         return cipher.doFinal(data);
     }
 }
